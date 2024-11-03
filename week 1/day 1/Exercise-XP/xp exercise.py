39c428b82e68fa465527ab5b1acf14dc54221586
gigi007-prog <gigi0okropiridze@gmail.com>
2024-11-03 14:40:52 +0200

    update again

---
+++ week 1/day 1/Exercise-XP/xp exercise.py
@@ -0,0 +1,51 @@
+#Exercise1
+print("Hello world\n"*4)
+
+#Exercise2
+print(int(99**3)*8)
+
+#Exercise3
+#5 < 3 outcome will be flase
+#3 == 3 outcome will be true
+#3 == "3" outcome will be flase
+#"3" > 3 outcome will be flase
+#"Hello" == "hello" outcome will be flase
+
+#Exercise4
+computer_brand="macbook"
+print("I have a " + computer_brand + " computer")
+
+#Exercises5
+name="Gigi"
+age="18"
+shoe_size="42"
+info="My name is " + name + " and I am " + age + " years old. I play tennis professionally and my shoe size is " + shoe_size 
+print(info)
+
+#Exercise6
+a=5
+b=1
+if a>b :
+    print("Hello World")
+
+#Exercise7
+number=int(input("Type a number;"))
+if number % 2==0:
+    print("This number is ever.")
+else:
+    print("This number is odd.")
+
+#Exercise8
+name=input("What's your name?")
+
+if name =="Gigi":
+    print("WOOWWWW we have the same nameeeeeee")
+else:
+    print("WOWWWWW our names might not match but your name is sooooo beautiful")
+
+#Exercise9
+height=int(input("How tall are you? in cm no need to write cm just write the number"))
+if height>145:
+    print("You're allowed to ride this rollercoaster")
+else:
+    print("You need to grow a bit more to ride this one kid")
