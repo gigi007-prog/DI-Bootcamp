70e5e48c1dad3353274199be7a76e0127ab4e7a7
gigi007-prog <gigi0okropiridze@gmail.com>
2024-11-11 18:36:57 +0200

    new update

--- week 2/day 2/xp_exercise2/xpexercise2.py
+++ week 2/day 2/xp_exercise2/xpexercise2.py
@@ -1,1 +1,48 @@
-pr
+#Exercise 1
+class Pets():
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+class Cat():
+    is_lazy = True
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def walk(self):
+        return f'{self.name} is just walking around'
+
+class Bengal(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+class Chartreux(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+class Siamese(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+bengal_cat=Bengal("Ryan",4 )
+charteux_cat=Chartreux("Poppy", 1)
+siamese_cat=Siamese("Hannah", 8)
+
+all_cats=[bengal_cat,charteux_cat,siamese_cat ]
+
+saras_pets=Pets(all_cats)
+
+saras_pets.walk()
+
+#Exercise 2
+class Dog():
+    def __init__(self, name, age, weight ):
+      self.name=name
+      self.age=age
+      self.weight=weight
+        
