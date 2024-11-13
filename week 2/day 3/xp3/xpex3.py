30967a67418781b99f930327714fa0b7519a67ee
gigi007-prog <gigi0okropiridze@gmail.com>
2024-11-13 10:04:36 +0200

    booo

--- week 2/day 3/xp3/xpex3.py
+++ week 2/day 3/xp3/xpex3.py
@@ -92,4 +92,23 @@ birthdate_input = "2006-04-19"
 minutes_lived(birthdate_input)
 
 #Exercise 7
+import faker
+users = []
+fake = Faker()
+
+def users_added():
+    user = {
+        'name': fake.name(), 
+        'address': fake.address(),
+        'language_code': fake.language_code()  
+    }
+    
+    users.append(user)
+
+users_added()   
+users_added()
+users_added()
+
+for er in users:
+    print(er)
 
