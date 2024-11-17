ac2d93095bd006ec4c3a9d6ac3c925e5a7bc6561
gigi007-prog <gigi0okropiridze@gmail.com>
2024-11-17 11:38:00 +0200

    data

---
+++ week 3/lecture.sql
@@ -0,0 +1,9 @@
+--THIS IS A COMMENT
+-- CREATE TABLE actors(
+-- actor_id SERIAL PRIMARY KEY,
+-- first_namem VARCHAR(50) NOT NULL,
+-- last_name VARCHAR(50) NOT NULL,
+-- age DATE NOT NULL,
+-- number_oscars SMALLINT NOT NULL)
+
+SELECT * FROM actors
