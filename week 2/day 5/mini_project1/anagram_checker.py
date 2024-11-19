382600d5bd66161173eaebb7caf4726e10d55e20
gigi007-prog <gigi0okropiridze@gmail.com>
2024-11-19 08:45:10 +0200

    mini projects

--- week 2/day 5/mini_project1/anagram_checker.py
+++ week 2/day 5/mini_project1/anagram_checker.py
@@ -1,1 +1,35 @@
-Ana
+class AnagramFinder:
+    def __init__(self, word_file):
+        try:
+            self.word_list = self.load_word_list_from_file(word_file)
+            # print("Loaded word list:")
+            # print(self.word_list)
+        except FileNotFoundError:
+            print(f"Error: The file '{word_file}' was not found.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    def load_word_list_from_file(self, word_file):
+        word_list = []
+        with open(word_file, 'r') as file: 
+            for line in file:
+                stripped_line = line.strip()  
+                if stripped_line:  
+                    word_list.append(stripped_line.lower())  
+        return word_list
+
+    def find_anagrams(self, target_word):
+        if not target_word:
+            print("Error: The target word is empty.")
+            return []
+
+        target_word_lower = target_word.lower()
+        sorted_target = ''.join(sorted(target_word_lower))  
+
+        anagrams = []
+
+        for word in self.word_list:
+            if ''.join(sorted(word)) == sorted_target and word != target_word_lower:
+                anagrams.append(word)
+
+        return anagrams
